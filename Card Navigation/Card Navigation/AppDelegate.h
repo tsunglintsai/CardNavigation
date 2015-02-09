38d6d81885e8ea47f13e996ff87e39dd6998bbe1
Henry Tsai <tsunglin.tsai@gmail.com>
2015-02-09 07:00:14 -0800

    Added Project files

---
+++ Card Navigation/Card Navigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Card Navigation
+//
+//  Created by Henry on 2/9/15.
+//  Copyright (c) 2015 Pyrogusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Card Navigation/Card Navigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Card Navigation
+//
+//  Created by Henry on 2/9/15.
+//  Copyright (c) 2015 Pyrogusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
